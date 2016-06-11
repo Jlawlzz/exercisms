e8fdb1b261e5ffba34b4672147f69ffbc4226697
Jordan Lawler <jordan.lawler@colorado.edu>
2016-06-10 22:06:51 -0600

    adjusted raindrop exercism

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -6,6 +6,7 @@ class Raindrops
     output += 'Pling' if (number % 3) == 0
     output += 'Plang' if (number % 5) == 0
     output += 'Plong' if (number % 7) == 0
+    
     output = number   if output == ''
 
     output.to_s
